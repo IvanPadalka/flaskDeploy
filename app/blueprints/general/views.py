26d8b786d9300963e1fa7548e3b75d458e01fe89
Joon699 <boichuko70@ukr.net>
2021-01-05 16:27:06 +0300

    Fixed main page

--- app/blueprints/general/views.py
+++ app/blueprints/general/views.py
@@ -3,4 +3,4 @@ from flask import Blueprint, render_template
 general_blueprint = Blueprint('general', __name__, template_folder='templates')
 @general_blueprint.route('/')
 def to_main():
-	return render_template('layout.html')
+	return render_template('general/layout.html')
--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=dt.utcnow)
     admin = db.Column(db.Boolean, default=False)
